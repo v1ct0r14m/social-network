9f4f1d42eb64f4dc48d4904e60f23301e943f1ee
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-15 23:27:23 -0600

    user controller created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,102 @@
-const { Shema, model } = require('mongoose')
+const { model } = require('mongoose')
+const { User, Thought } = require('models')
 
 const userController = {
+
     getUsers(req, res) {
-        User.find({}).populate({
-            path: 'thoughts'
+        User.find({}).then(userData => {
+            console.log(userData)
+            res.json(userData)
         })
-        .populate({
-            path: 'friends'
+        if (err) throw err
+        res.json(err)    
+    },
+    getUserById(req, res) {
+        User.findOne({_id: req.params.userId})
+        .then(userData => {
+            console.log(userData)
+            if(!userData) {
+            res.status(404).json({message: 'user not found'})
+        }
+        res.json(userData)
         })
-        .select
+        if (err) throw err
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate({_id: req.params.userId},
+            req.body,
+            {new.true}
+        )
+        .then(userData => {
+            console.log(userData)
+            if(!userData){
+                res.status(404).json({message: 'user not found'})
+            }
+            res.json(userData)
+        })
+        if (err) throw err
+    },
+    createUser(req, res) {
+        User.createUser(req.body).then(userData => {
+            console.log(userData)
+            res.json(userData)
+        })
+        if (err) throw err
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.userId})
+        .then(userData => {
+            console.log(userData)
+            if(!userData){
+                res.status(404).json({message: 'user not found'})
+                return
+            }
+            if(userData.Thought) {
+                console.log('user has thoughts')
+                return Thought.deleteMany({username: userData, username})
+                .then(() => {
+                    res.json(userData)
+                })
+            } else {
+                res.json(userData)
+            }
+        })
+        if (err) throw err
+    },
+    
+    addFriend(req, res) {
+        user.findOneAndUpdate(
+            {_id: req.params.userId},
+            {push: {friend: req.params.friendId}},
+            {new: true}
+        )
+        .then(userData => {
+            console.log(userData)
+            if(!userData){
+                res.status(404).json({message: 'user not found'})
+                return
+            }
+            res.json(userData)
+        })
+        if (err) throw err
+    },
+    
+    deleteFriend(req, res) {
+        user.findOneAndUpdate(
+            {_id: req.params.userId},
+            {pull: {friend: req.params.friendId}},
+            {new: true}
+        )
+        .then(userData => {
+            console.log(userData)
+            if(!userData){
+                res.status(404).json({message: 'user not found'})
+                return
+            }
+            res.json(userData)
+        })
+        if (err) throw err
     }
 }
+
+module.exports = userController
