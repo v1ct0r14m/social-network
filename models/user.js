7344dddda97e6b90407827df473352273f27cefa
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-15 23:44:25 -0600

    user schema created

--- models/user.js
+++ models/user.js
@@ -24,8 +24,16 @@ const UserSchema = new Schema({
         ref: 'User'
         }
     ]
-})
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+    })
 
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+})
 
 const User = model('User', UserSchema)
 
